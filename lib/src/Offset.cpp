0887e1a158b018abc1e13c6c9d54bc8fcd0bc724
Tom Kent <teeks99@yahoo.com>
2013-01-19 15:17:29 -0400

    Setup offset object

---
+++ lib/src/Offset.cpp
@@ -0,0 +1,35 @@
+/*
+    Copyright 2013 Thomas Kent
+*/
+
+/************************************************************************ 
+This file is part of Falcon Time.
+
+    Falcon Time is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 3 of the 
+    License, or (at your option) any later version.
+
+    Falcon Time is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Falcon Time.  If not, see <http://www.gnu.org/licenses/>.
+************************************************************************/
+
+#include "Offset.h"
+
+using namespace FalconTime;
+
+// TODO: This is not thread-safe and should be! We need to add mutexes or
+// fancy ways of flushing cache between reads and writes.
+
+void Offset::set_offset(int64_t value){
+    _offset = value;
+}
+
+int64_t Offset::get_offset(){
+    return _offset;
+}
